55014e0e5dc542e16c4f52e93db9b871ca015a3f
AugustoCesar20 <79537628+AugustoCesar20@users.noreply.github.com>
2023-06-22 14:58:49 -0300

    Update lightsensor_client.py

--- CMU-StarterProject-main/ClientCode/python/lightsensor_client.py
+++ CMU-StarterProject-main/ClientCode/python/lightsensor_client.py
@@ -12,7 +12,7 @@ from const import *
 def run():
     with grpc.insecure_channel(GRPC_SERVER+':'+GRPC_PORT) as channel:
         stub = iot_service_pb2_grpc.IoTServiceStub(channel)
-        response = stub.SayLightLevel(iot_service_pb2.LightLevelRequest(sensorName='my_sensor', username='user', password='senha')) #depois modificar usuario
+        response = stub.SayLightLevel(iot_service_pb2.LightLevelRequest(sensorName='my_sensor'))
 
     print("Light level received: " + response.lightLevel)
 
