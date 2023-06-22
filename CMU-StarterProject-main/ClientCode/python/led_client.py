dbdb542a13f86889546c2c727467f45e53eaa72e
AugustoCesar20 <79537628+AugustoCesar20@users.noreply.github.com>
2023-06-22 14:58:22 -0300

    Update led_client.py

--- CMU-StarterProject-main/ClientCode/python/led_client.py
+++ CMU-StarterProject-main/ClientCode/python/led_client.py
@@ -13,7 +13,7 @@ from const import *
 def run():
     with grpc.insecure_channel(GRPC_SERVER+':'+GRPC_PORT) as channel:
         stub = iot_service_pb2_grpc.IoTServiceStub (channel)
-        response = stub.BlinkLed(iot_service_pb2.LedRequest(state=int(sys.argv[1]),ledname=sys.argv[2], username='user', password='senha')) #depois modificar usuario
+        response = stub.BlinkLed(iot_service_pb2.LedRequest(state=int(sys.argv[1]),ledname=sys.argv[2], username=sys.argv[3], password=sys.argv[4]))
 
     if response.ledstate[sys.argv[2]] == 1:
         print("Led state is on")
